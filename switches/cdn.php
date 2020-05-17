f153613df83772270ff75791931920f57eceba42
Amish Dotcom <Amish Hacker@Lovely.Amish_Dotcom>
2020-05-17 21:29:26 +0530

    Add all my files

--- switches/cdn.php
+++ switches/cdn.php
@@ -5,8 +5,8 @@ $site_name = "flixblade";
 $site_tagline = "The Media Database";
 $site_root = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 $current_page_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$cdn = "http://127.0.0.1/cdn/flixblade";
-$cdn_loc = "../../../cdn/flixblade"; //To address the same both the $cdn and $cdn_loc, one for http and the other one for local files use from the perspective of trigger page.
+$cdn = "http://127.0.0.1/flixblade";
+$cdn_loc = "../../../../cdn/flixblade"; //To address the same both the $cdn and $cdn_loc, one for http and the other one for local files use from the perspective of trigger page.
 $custom_site_root = "flixblade.localhost";
 
 //Url Files Variables
